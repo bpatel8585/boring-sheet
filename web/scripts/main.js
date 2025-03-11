232021da45a183458c84607c3b057f70b2971e25
bpatel8585 <bharat.patel85@gmail.com>
2025-03-11 10:38:04 -0800

    enables VR button

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -1,4 +1,7 @@
 import * as THREE from 'three';
+import WebGL from 'three/addons/capabilities/WebGL.js';
+import { VRButton } from 'three/addons/webxr/VRButton.js';
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -6,14 +9,15 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+document.body.appendChild( VRButton.createButton( renderer ) );
 
 if ( WebGL.isWebGL2Available() ) {
-	animate();
+	renderer.setAnimationLoop( function () {
+
+		renderer.render( scene, camera );
+	
+	} );
 } else {
 	const warning = WebGL.getWebGL2ErrorMessage();
 	document.getElementById( 'container' ).appendChild( warning );
-}
-
-function animate() {
-	renderer.render( scene, camera );
 }
